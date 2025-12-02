518a233047645adb58418e514a45ec96530acf1b
Dmiliate2 <100120272+Dmiliate2@users.noreply.github.com>
2025-12-02 15:20:10 -0800

    Delete figure directory

--- figure/spearman相关性.py
+++
@@ -1,6 +0,0 @@
-from scipy.stats import spearmanr
-import pandas as pd
-
-data = pd.read_csv('A_ESOL_FG_SAR_OPT.csv')
-spearman_result = spearmanr(data['attribution'], data['pred_mean'])
-print(spearman_result)
